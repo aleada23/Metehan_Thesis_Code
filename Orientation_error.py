6190d39aff692c1666b8a3f0e7bb819f85adc3a8
Alessandro Adami <131950430+aleada23@users.noreply.github.com>
2025-03-13 15:16:56 +0100

    Update Orientation_error.py

--- Orientation_error.py
+++ Orientation_error.py
@@ -3,6 +3,6 @@
 #The unit quaternions are defined as q = np.array([x, y, z, w])
 
 #Compute the Skew-Symm. matrix for the desired orientation
-skew_des = np.array([[int(0), -actual_target_quat[2], actual_target_quat[1]], [actual_target_quat[2], int(0) , -actual_target_quat[0]], [-actual_target_quat[1], actual_target_quat[0], int(0)]])
+skew_des = np.array([[int(0), -target_quat[2], target_quat[1]], [target_quat[2], int(0) , -target_quat[0]], [-target_quat[1], target_quat[0], int(0)]])
 #Compute the orientation error
-orientation_err = (actual_quat[3]*np.array([actual_target_quat[0], actual_target_quat[1], actual_target_quat[2]])) - (actual_target_quat[3]*np.array([actual_quat[0], actual_quat[1], actual_quat[2]])) - np.matmul(skew_des,np.array([actual_quat[0], actual_quat[1], actual_quat[2]])) 
+orientation_err = (actual_quat[3]*np.array([target_quat[0], target_quat[1], target_quat[2]])) - (target_quat[3]*np.array([actual_quat[0], actual_quat[1], actual_quat[2]])) - np.matmul(skew_des,np.array([actual_quat[0], actual_quat[1], actual_quat[2]])) 
